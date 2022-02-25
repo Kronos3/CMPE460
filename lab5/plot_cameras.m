07ab2b95a72ed36a75a62df6905e79176bc630a0
Andrei Tumbar <andreitumbar@gmail.com>
2022-02-24 23:47:19 -0500

    Update plot_cameras.m

--- lab5/plot_cameras.m
+++ lab5/plot_cameras.m
@@ -5,56 +5,55 @@
 %
 %
 
-function plot_cams
 
 %Send over bluetooth or serial
-serialPort = '/dev/tty.KeySerial1';
-serialObject = serial(serialPort);
-%configure serial connection
-serialObject.BaudRate = 115200; %(Default)
-%serialObject.BaudRate = 115200;
-%serialObject.FlowControl = 'software';
+%serialPort = '/dev/tty.usbmodemM43210051'
+%clear serialObject;
 
-%Initiate serial connection
-fopen(serialObject);
-
-% This gets called on cleanup to ensure the stream gets closed
-finishup = onCleanup(@() myCleanupFun(serialObject));
+function plot_cams(serialObject)
 
 % Instantiate variables
 count = 1;
 trace = zeros(1, 128); %Stored Values for Raw Input
-bintrace = zeros(1,128); %Stored Values for Edge Detection
-smoothtrace  = zeros(1,128); %Stored Values for 5-Point Averager
 
 while (1)
     % Check for data in the stream
     if serialObject.BytesAvailable
         val = fscanf(serialObject,'%i');
-        %val
-        if ((val == -1) || (val == -3)) % -1 and -3 are start keywords
+        if (val == -1) % -1 and -3 are start keywords
             count = 1;
         elseif (val == -2) % End camera1 tx
-            if (count == 128)
-                plot_cameras(trace, 1);
-            end %otherwise there was an error and don't plot
+            if (count == 129)
+                plot_cameras(trace);
+            end
             count = 1;
-            %plotdata(trace);
-        elseif (val == -4) % End camera2 tx
-            count = 1;
-            plot_cameras(trace, 2);
         else
-            trace(count) = val;
-            count = count + 1;
+            if (count <= 128)
+                trace(1,count) = val;
+                count = count + 1;
+            end
         end % if
     end %bytes available
 end % while(1)
 
+end
+
+function [serialOut] = open_serial(path)
+    serialOut = serial(path);
+    serialOut.BaudRate = 115200;
+    fopen(serialOut);
+end
+
+function close_serial(ser)
+    fclose(serialObject);
+    delete(serialObject);
+    clear serialObject;
+end
+
 %*****************************************************************************************************************
 %*****************************************************************************************************************
 
-function plot_cameras
-trace = [2062 2127 2283 2358 2502 2574 2744 2817 3040 3048 3186 3228 3432 3424 3574 3625 3740 3796 3887 3887 4023 4036 4162 4177 4259 4193 4357 4331 4453 4385 4522 4489 4521 4547 4571 4544 4613 4456 4363 4157 4018 3796 3541 3299 3123 2867 2675 2417 2247 1959 1809 1586 1446 1250 1153 1068 1072 1048 1097 1038 1061 1038 1064 1054 1056 1046 1027 1018 1064 1011 1040 1009 1050 999 1039 990 1009 999 1023 1031 1032 1016 1028 1030 1048 1026 1066 1020 1045 1011 1020 1025 1035 1030 1041 1012 1023 1001 1014 970 984 951 959 915 917 905 904 869 866 827 855 823 847 781 807 773 778 752 773 725 727 697 718 697 695 673 670 670];
+function plot_cameras(trace)
 
 drawnow;
 figure(1)
